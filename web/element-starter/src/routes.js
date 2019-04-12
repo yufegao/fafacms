f08b8a03d6f44b821c634d8b7fd18646cc5f0248
hunterhug <gdccmcm14@live.com>
2019-04-12 20:26:12 +0800

    vue api

---
+++ web/element-starter/src/routes.js
@@ -0,0 +1,12 @@
+import Login from './views/Login.vue'
+
+let routes = [
+    {
+        path: '/login',
+        component: Login,
+        name: '',
+        hidden: true
+    }
+];
+
+export default routes;
